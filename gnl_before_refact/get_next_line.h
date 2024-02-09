9d1667d6bdd7725652dceabe2a23314120c5c595
Wachiii_q <wachiraya.chu@student.mahidol.edu>
2024-02-10 02:20:56 +0700

    wip: still not work

--- gnl_before_refact/get_next_line.h
+++ gnl_before_refact/get_next_line.h
@@ -6,34 +6,34 @@
 /*   By: wchumane <wchumane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/20 12:30:20 by wchumane          #+#    #+#             */
-/*   Updated: 2024/01/21 18:00:41 by wchumane         ###   ########.fr       */
+/*   Updated: 2024/02/09 23:04:38 by wchumane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# ifndef GET_NEXT_LINE
-# define GET_NEXT_LINE
+// # ifndef GET_NEXT_LINE
+// # define GET_NEXT_LINE
 
-# ifndef BUFFER_SIZE
-# define BUFFER_SIZE 10
-# endif
-
-
-#include <stdio.h>
-#include <stdlib.h>
-#include <fcntl.h>
-#include <unistd.h>
+// # ifndef BUFFER_SIZE
+// # define BUFFER_SIZE 10
+// # endif
 
 
-size_t		ft_strlen(const char *str);
-size_t		ft_strlcpy(char *dst, const char *src, size_t size);
-char		*ft_strdup(const char *s1);
-char	*ft_strrchr(const char *s, int c);
-
-char		*ft_strjoin(char *s1, char const *s2);
-char		*ft_substr(char const *s, unsigned int start, size_t len);
-char	    *clear_array(char *str, size_t length);
-char		*read_line(int fd, char *str_array, char *buffer);
-char		*get_next_line(int fd);
+// #include <stdio.h>
+// #include <stdlib.h>
+// #include <fcntl.h>
+// #include <unistd.h>
 
 
-# endif
+// size_t		ft_strlen(const char *str);
+// size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+// char		*ft_strdup(const char *s1);
+// char	*ft_strrchr(const char *s, int c);
+
+// char		*ft_strjoin(char *s1, char const *s2);
+// char		*ft_substr(char const *s, unsigned int start, size_t len);
+// char	    *clear_array(char *str, size_t length);
+// char		*read_line(int fd, char *str_array, char *buffer);
+// char		*get_next_line(int fd);
+
+
+// # endif
--- gnl_before_refact/get_next_line_utils.c
+++ gnl_before_refact/get_next_line_utils.c
@@ -6,93 +6,93 @@
 /*   By: wchumane <wchumane@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/21 15:32:06 by wchumane          #+#    #+#             */
-/*   Updated: 2024/01/21 20:49:16 by wchumane         ###   ########.fr       */
+/*   Updated: 2024/02/10 01:25:15 by wchumane         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-# include "get_next_line.h"
+// # include "get_next_line.h"
 
-size_t	ft_strlen(const char *str)
-{
-	int	counter;
+// size_t	ft_strlen(const char *str)
+// {
+// 	int	counter;
 
-	counter = 0;
-	while (str[counter] != '\0')
-	{
-		counter++;
-	}
-	return (counter);
-}
+// 	counter = 0;
+// 	while (str[counter] != '\0')
+// 	{
+// 		counter++;
+// 	}
+// 	return (counter);
+// }
 
-size_t	ft_strlcpy(char *dst, const char *src, size_t size)
-{
-	size_t	i;
+// size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+// {
+// 	size_t	i;
 
-	i = 0;
-	if (size > 0)
-	{
-		while (src[i] && i < (size - 1))
-		{
-			dst[i] = src[i];
-			i++;
-		}
-		dst[i] = 0;
-	}
-	while (src[i])
-		i++;
-	return (i);
-}
+// 	i = 0;
+// 	if (size > 0)
+// 	{
+// 		while (src[i] && i < (size - 1))
+// 		{
+// 			dst[i] = src[i];
+// 			i++;
+// 		}
+// 		dst[i] = 0;
+// 	}
+// 	while (src[i])
+// 		i++;
+// 	return (i);
+// }
 
-char	*ft_strdup(const char *s1)
-{
-	char	*ptr;
-	size_t	len;
+// char	*ft_strdup(const char *s1)
+// {
+// 	char	*ptr;
+// 	size_t	len;
 
-	len = ft_strlen(s1) + 1;
-	ptr = malloc(len);
-	if (!ptr)
-		return (NULL);
-	ft_strlcpy(ptr, s1, len);
-	return (ptr);
-}
+// 	len = ft_strlen(s1) + 1;
+// 	ptr = malloc(len);
+// 	if (!ptr)
+// 		return (NULL);
+// 	ft_strlcpy(ptr, s1, len);
+// 	return (ptr);
+// }
 
-char	*ft_strrchr(const char *s, int c)
-{
-	int				i;
-	unsigned char	d;
+// char	*ft_strrchr(const char *s, int c)
+// {
+// 	int				i;
+// 	unsigned char	d;
 
-	d = c;
-	i = ft_strlen(s);
-	if (d == '\0')
-	{
-		return ((char *)s + (i));
-	}
-	while (i >= 0)
-	{
-		if (s[i] == d)
-			return ((char *)s + i);
-		i--;
-	}
-	return (NULL);
-}
+// 	d = c;
+// 	i = ft_strlen(s);
+// 	if (d == '\0')
+// 	{
+// 		return ((char *)s + (i));
+// 	}
+// 	while (i >= 0)
+// 	{
+// 		if (s[i] == d)
+// 			return ((char *)s + i);
+// 		i--;
+// 	}
+// 	return (NULL);
+// }
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*ptr;
-	size_t	i;
+// char	*ft_substr(char const *s, unsigned int start, size_t len)
+// {
+// 	char	*ptr;
+// 	size_t	i;
 
-	i = 0;
-	if (!s)
-		return (NULL);
-	if (ft_strlen(s) < start)
-		return (ft_strdup(""));
-	if (ft_strlen(s) - start < len)
-		len = ft_strlen(s) - start;
-	ptr = malloc(len + 1);
-	if (!ptr)
-		return (NULL);
-	while (i < len && s[start])
-		ptr[i++] = s[start++];
-	ptr[i] = '\0';
-	return (ptr);
-}
+// 	i = 0;
+// 	if (!s)
+// 		return (NULL);
+// 	if (ft_strlen(s) < start)
+// 		return (ft_strdup(""));
+// 	if (ft_strlen(s) - start < len)
+// 		len = ft_strlen(s) - start;
+// 	ptr = malloc(len + 1);
+// 	if (!ptr)
+// 		return (NULL);
+// 	while (i < len && s[start])
+// 		ptr[i++] = s[start++];
+// 	ptr[i] = '\0';
+// 	return (ptr);
+// }
