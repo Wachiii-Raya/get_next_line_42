948c3e1af2154f9868c3aa353e9958a76caa1ec0
Wachiii_q <wachiraya.chu@student.mahidol.edu>
2024-02-10 02:19:16 +0700

    wip: reduce line; still leak

---
+++ refactor_gnl/get_next_line.h
@@ -0,0 +1,39 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchumane <wchumane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 23:07:31 by wchumane          #+#    #+#             */
+/*   Updated: 2024/02/10 00:28:06 by wchumane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# ifndef GET_NEXT_LINE
+# define GET_NEXT_LINE
+
+# ifndef BUFFER_SIZE
+# define BUFFER_SIZE 10
+# endif
+
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <unistd.h>
+
+
+size_t		ft_strlen(const char *str);
+size_t		ft_strlcpy(char *dst, const char *src, size_t size);
+char		*ft_strdup(const char *s1);
+char	*ft_strrchr(const char *s, int c);
+
+char		*ft_strjoin(char *s1, char const *s2);
+char		*ft_substr(char const *s, unsigned int start, size_t len);
+char	    *clear_array(char *str, size_t length);
+char		*read_line(int fd, char *str_array, char *buffer);
+char		*get_next_line(int fd);
+
+
+# endif
---
+++ refactor_gnl/get_next_line_utils.c
@@ -0,0 +1,98 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_utils.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: wchumane <wchumane@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/09 23:07:02 by wchumane          #+#    #+#             */
+/*   Updated: 2024/02/10 00:46:57 by wchumane         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include "get_next_line.h"
+
+size_t	ft_strlen(const char *str)
+{
+	int	counter;
+
+	counter = 0;
+	while (str[counter] != '\0')
+	{
+		counter++;
+	}
+	return (counter);
+}
+
+size_t	ft_strlcpy(char *dst, const char *src, size_t size)
+{
+	size_t	i;
+
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < (size - 1))
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = 0;
+	}
+	while (src[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strdup(const char *s1)
+{
+	char	*ptr;
+	size_t	len;
+
+	len = ft_strlen(s1) + 1;
+	ptr = malloc(len);
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, s1, len);
+	return (ptr);
+}
+
+char	*ft_strrchr(const char *s, int c)
+{
+	int				i;
+	unsigned char	d;
+
+	d = c;
+	i = ft_strlen(s);
+	if (d == '\0')
+	{
+		return ((char *)s + (i));
+	}
+	while (i >= 0)
+	{
+		if (s[i] == d)
+			return ((char *)s + i);
+		i--;
+	}
+	return (NULL);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	i;
+
+	i = 0;
+	if (!s)
+		return (NULL);
+	if (ft_strlen(s) < start)
+		return (ft_strdup(""));
+	if (ft_strlen(s) - start < len)
+		len = ft_strlen(s) - start;
+	ptr = malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	while (i < len && s[start])
+		ptr[i++] = s[start++];
+	ptr[i] = '\0';
+	return (ptr);
+}
